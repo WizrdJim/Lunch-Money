03756166da42cd3d680653cef8339dcdeab2adb2
WizrdJim <wzrdjim@gmail.com>
2018-01-05 10:27:22 -0500

    Added redux to index.js

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './App';
 import './index.css';
+import reducers from './reducers';
+
+
+const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
+const store = createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+  <Provider store = { store }>
+    <Router>
+      <App />
+    </Router>
+  </Provider>, document.getElementById('root'));
